b133ef28d32c103808a9001b1d32da3a0001481e
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-08-11 01:05:35 -0500

    Correccion ruta controlador

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var app = express();
 // app.listen(port);
 // console.log('Listening on port 3000...');
 // const routes = require('./routes')
-const MongoClient = require('mongodb').MongoClient;
+
+
+
+/*const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.MONGODB_URI;
 // "mongodb+srv://user-mongodb:apirestul1@cluster0.3d32f.mongodb.net/libreria?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,13 +19,14 @@ client.connect(err => {
   // perform actions on the collection object
   console.log("con",err)
   client.close();
-});
+});*/
 
-/*var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var mongoDB = "mongodb+srv://user-mongodb:apirestul1@cluster0.3d32f.mongodb.net/libreria?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));*/
+console.log ("DBBBBBBb", db)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 //connect to mongoose
 // to avoid deprecation warning, provide a second argument {useNewUrlParser: true, useUnifiedTopology: true}
 // mongoose.connect(
