f97bb5bf21d71e0d625d6b6d99d24a7edc0c300f
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-08-11 03:26:05 -0500

    funvci

--- database/conexion.js
+++ database/conexion.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const URI ="mongodb+srv://user-mongodb:1234@cluster0.3d32f.mongodb.net/libreria?retryWrites=true&w=majority";
 
-const connectDB = () => {
-  mongoose.connect(URI, {
+const connectDB = async () => {
+  await mongoose.connect(URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true
   });
