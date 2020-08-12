b0459e240f4212f7300123d466a6b1f99cd2694b
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-08-12 03:11:36 -0500

    api préstamos

--- api/prestamo.js
+++ api/prestamo.js
@@ -27,7 +27,7 @@ module.exports = {
     },
 
     verTodos: async (req, res) => {
-        const prestamo = await Prestamo.find().populate({path: 'libros', select: 'titulo' }).populate('usuario');
+        const prestamo = await Prestamo.find().populate('usuario');
         return res.send(prestamo)
     },
 }
