919ee16506fe27755c12c3080b169ab1af3a4121
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-08-12 01:18:15 -0500

    api préstamos

--- api/prestamo.js
+++ api/prestamo.js
@@ -24,44 +24,9 @@ module.exports = {
             }
         });
     },
-        
-
-    // create : async (req, res) => {
-    //     autor = req.params;
-    //     id = autor.id;
-    //     const { title, summary, isbn} = req.body;
-    //     const libro = await Libro.create({
-    //         title,
-    //         summary,
-    //         isbn,
-    //         autor:id
-    //     });
-    //     await libro.save();
-    //     const autorById = await Autor.findById(id);
-    //     autorById.libros.push(libro);
-    //     await autorById.save();
-    //     return res.send(autorById);
-    // },
-
-    // create : async (req, res) => {
-    //     console.log(req.params);
-    //     user = req.params;
-    //     id = user.id;
-    //     const { title, subtitle} = req.body;
-    //     const prestamoModel = await Prestamo.create({
-    //         fecha,
-    //         usuario:id,
-    //         libro:id
-    //     });
-    //     await prestamoModel.save();
-    //     const userById = await User.findById(id);
-    //     userById.posts.push(post);
-    //     await userById.save();
-    //     return res.send(userById);
-    // },
 
     verTodos: async (req, res) => {
-        const prestamo = await Prestamo.find()
+        const prestamo = await Prestamo.find().populate("libros")
         return res.send(prestamo)
     },
 }
