069653bdcc919db8b2c2d4204c785fb76c2405ab
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-09-22 19:51:03 -0500

    Cambio de servidor

--- routes.js
+++ routes.js
@@ -6,19 +6,19 @@ const Libro = require('./api/libro');
 const Autor = require('./api/autor');
 const Genero = require('./api/genero');
 
-router.get('/usuarios/consultar', Usuario.verTodos);
-router.get('/usuario/buscar/:cedula', Usuario.buscarPorId);
-router.post('/usuario/crear', Usuario.crear);
-router.put('/usuario/modificar/:id', Usuario.modificar);
-router.delete('/usuario/eliminar/:id', Usuario.eliminar);
+router.get('/usuarios', Usuario.verTodos);
+router.get('/buscar_usuario/:cedula', Usuario.buscarPorId);
+router.post('/crear_usuario', Usuario.crear);
+router.put('/modificar_usuario/:id', Usuario.modificar);
+router.delete('/eliminar_usuario/:id', Usuario.eliminar);
 
-router.get('/autores/consultar', Autor.verTodos);
+router.get('/autores', Autor.verTodos);
 router.post('/autor/crear', Autor.crear);
 
 router.get('/generos/consultar', Genero.verTodos);
 router.post('/genero/crear', Genero.crear);
 
-router.get('/libros/consultar', Libro.verTodos);
+router.get('/libros', Libro.verTodos);
 router.post('/libro/crear', Libro.crear);
 
 router.get('/prestamos/consultar', Prestamo.verTodos);
--- server.js
+++ server.js
@@ -4,21 +4,35 @@ const mongoose = require('mongoose');
 const app = express();
 
 //connectDB();
-const URI = 'mongodb+srv://user-mongodb:1234@cluster0.3d32f.mongodb.net/libreria';
-//"mongodb+srv://user-mongodb:1234@cluster0.3d32f.mongodb.net/libreria?retryWrites=true&w=majority";
+const URI ="mongodb+srv://user-mongodb:1234@cluster0.3d32f.mongodb.net/libreria?retryWrites=true&w=majority";
+//'mongodb+srv://user-mongodb:1234@cluster0.3d32f.mongodb.net/libreria';
+//
 
-const connectDB = async () => {
-    await mongoose.connect(URI, {
+// const connectDB = async () => {
+//     await 
+// };
+//const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://usermongodb:1234@cluster0.3d32f.mongodb.net/libreria";
+// const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("libreria").collection("libros");
+//   // perform actions on the collection object
+//   console.log(err.message)
+//   client.close();
+// }); 
+try{
+    mongoose.connect(URI, {
         useUnifiedTopology: true,
-        useNewUrlParser: true
+        useNewUrlParser: true,
     });
-    console.log('¡Conectado a la base de datos!');
-};
-connectDB();
+    mongoose.set('useCreateIndex', true);
+    console.log('¡Conectado a la base de datos!')
+}catch(err){
+    console.log("ERROR de conexión")
+}
+
 app.use(express.json({ extended: false }));
 app.use(require('./routes'));
-const Port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(Port, () => console.log('Server started'));
-// .listen(process.env.PORT, '0.0.0.0');
-// .
+app.listen(port);
