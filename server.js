b2d0ba902f74529ccb9d268df234cbd668d21283
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-09-24 20:39:42 -0500

    Deploy to Heroku

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const app = express();
 const URI = "mongodb+srv://user-mongodb:1234@cluster0.3d32f.mongodb.net/libreria?retryWrites=true&w=majority";
 //'mongodb+srv://user-mongodb:1234@cluster0.3d32f.mongodb.net/libreria';
 
-  mongoose.connect(URI, 
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    },
-    console.log("¡Conectado a la base de datos!"),
-    mongoose.set('useCreateIndex', true),
-    mongoose.set('useFindAndModify', false)
-  ).catch(error => console.log(error,"Error de conexión"));
+mongoose.connect(URI, 
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  console.log("¡Conectado a la base de datos!"),
+  mongoose.set('useCreateIndex', true),
+  mongoose.set('useFindAndModify', false)
+).catch(error => console.log(error,"Error de conexión"));
 
 app.use(express.json({ extended: false }));
 app.use(require('./routes'));
